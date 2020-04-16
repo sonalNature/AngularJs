48ea3d2e3675aca42e8756dcd033dc89b4659182
Sonal Singh <sonal.nature@gmail.com>
2020-04-15 17:09:49 -0700

    Adding some file this is my frst angular project commit

---
+++ Custom.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+(function () {
+    angular.module('FirstModule', [])
+        .controller('FirstController', function ($scope) {
+            $scope.name="Sonal"
+        })
+
+
+}) ();
